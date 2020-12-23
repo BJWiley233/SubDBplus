0905656a4dbe26c9961759b2fab2dde961621328
BJWiley23 <bwiley4@jh.edu>
2020-12-23 16:21:43 -0500

    Update mysql_conn_test.py

--- python_scripts/old_stuff/mysql_conn_test.py
+++ python_scripts/old_stuff/mysql_conn_test.py
@@ -1,6 +1,6 @@
 import mysql.connector
 
-cnx = mysql.connector.connect(user='root', password='Swimgolf1212**',
+cnx = mysql.connector.connect(user='root', password='**',
                               host='127.0.0.1',
                               database='merops')
 cursor = cnx.cursor()   
