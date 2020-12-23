3025d57ffd0c242a4f7b4dfc6774969f09a723bb
BJWiley23 <bwiley4@jh.edu>
2020-12-23 16:08:13 -0500

    Update app.R

--- applicationFinal/app.R
+++ applicationFinal/app.R
@@ -19,7 +19,7 @@ loadData <- function(query) {
   db <- RMySQL::dbConnect(RMySQL::MySQL(),
                           db = "protTest",
                           username = "root",
-                          password = "Swimgolf1212**",
+                          password = "**",
                           host = "127.0.0.1")
   dat <- dbGetQuery(db, query)
   dbDisconnect(db)
