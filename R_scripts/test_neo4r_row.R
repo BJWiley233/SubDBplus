031d716a9c72398a7c782f1fc8b0c3e044712862
BJWiley23 <bwiley4@jh.edu>
2020-12-23 16:16:51 -0500

    Update test_neo4r_row.R

--- R_scripts/test_neo4r_row.R
+++ R_scripts/test_neo4r_row.R
@@ -11,7 +11,7 @@ library(plotly)
 
 con <- neo4j_api$new(url = "http://localhost:7474", 
                      db = "protTest", user = "neo4j", 
-                     password = "Swimgolf1212**", isV4 = TRUE)
+                     password = "**", isV4 = TRUE)
 status_code(GET("http://localhost:7474"))
 
 
